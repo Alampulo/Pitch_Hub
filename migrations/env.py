1a6fb170aa1e41c15e91a67ac9c7fda49b3a0e1b
Bryan Juma <jumabryan10@gmail.com>
2018-09-06 12:50:08 +0300

    importing sqlalchemy

---
+++ migrations/env.py
@@ -0,0 +1,4 @@
+from flask import current_app
+config.set_main_option('sqlalchemy.url',
+                       current_app.config.get('SQLALCHEMY_DATABASE_URI'))
+target_metadata = current_app.extensions['migrate'].db.metadata
